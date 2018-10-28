890a3185f21193a4497b997f55cd2a634f6c51d5
Richard Hill <richardqhill@gmail.com>
2018-10-28 02:27:13 -0400

    threading is possibly working. cleaned up some warnings

--- proxy.c
+++ proxy.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <limits.h>
 #include "csapp.h"
 
 /* Recommended max cache and object sizes */
@@ -12,27 +13,52 @@ static const char *connection_hdr = "Connection: close\r\n";
 static const char *proxy_conn_hdr = "Proxy-Connection: close\r\n";
 
 /* Struct for populating the content from URI */
-struct req_content {
+struct uri_content {
     char host[MAXLINE];
     char path[MAXLINE];
     char port[MAXLINE];
 };
 
+typedef struct cache_object {
+    char uri[MAXLINE];
+    char data[MAX_OBJECT_SIZE];
+    int timestamp; //necessary?
+    pthread_rwlock_t rwlock;
+    struct cache_object *next;
+}cache_object;
+
+int cache_size = 0, timer = 0;
+cache_object *cache_start; /* Head pointer for cache list */
+pthread_rwlock_t cache_start_rwlock; /* Lock to synchronise the adding of new blocks to the cache */
 
 void doit(int connfd);
-void parse_uri(char *uri, struct req_content *content, bool* is_dynamic);
+void parse_uri(char *uri, struct uri_content *content, bool* is_dynamic);
 bool read_requesthdrs(rio_t *rp, char *header_buf);
-void clienterror(int fd, char *cause, char *errnum,
-                 char *shortmsg, char *longmsg);
+cache_object *check_cache_hit(char *uri);
+void read_cache_data(cache_object *cp, char *response);
+void write_to_cache(char *tag, char *data, int size);
+void thread_wrapper(void *vargs);
+void sig_handler(int sig);
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 
 
 
 int main(int argc, char **argv)
 {
-    int listenfd, connfd;
+    Signal(SIGPIPE, sig_handler); // Ignore SIGPIPE
+
+    int listenfd;
+    int* connfd;
     char hostname[MAXLINE], port[MAXLINE];
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
+    clientlen = sizeof(clientaddr);
+
+    pthread_t tid;
+    cache_start = NULL;
+
+    //Initialize lock for cache_start to maintain thread-safe operation
+    pthread_rwlock_init(&(cache_start_rwlock), NULL);
 
     /* Check command line args */
     if (argc != 2) {
@@ -42,27 +68,29 @@ int main(int argc, char **argv)
 
     listenfd = Open_listenfd(argv[1]);
     while (1) {
-        clientlen = sizeof(clientaddr);
-        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        connfd = (int *) Malloc(sizeof(connfd));
+        *connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
         Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE,
                     port, MAXLINE, 0);
         printf("Accepted connection from (%s, %s)\n", hostname, port);
-        doit(connfd);
-        Close(connfd);
+
+        Pthread_create(&tid, NULL, (void *)thread_wrapper, connfd);
     }
+    return 0;
 }
 
 
 void doit(int connfd) {
 
     char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
-    char hdr_data[MAXLINE], new_request[MAXBUF], response[1<<20]; /*Is there a max response size? Maybe max object *2?*/
+    char hdr_data[MAXLINE], new_request[MAXBUF], response[1<<16]; /*Is there a max response size? */
     rio_t rio;
-    struct req_content content;
+    struct uri_content content;
     int clientfd;
-    bool is_dynamic, host_mentioned;
+    bool is_dynamic, host_hdr_recvd;
 
-    //timer++;
+    /* Increment timer with every request for cache purposes */
+    timer++;
 
     /* Read request line and headers */
     Rio_readinitb(&rio, connfd);
@@ -71,7 +99,7 @@ void doit(int connfd) {
     printf("%s\n", buf);
     sscanf(buf, "%s %s %s", method, uri, version);
 
-    /* Proxy returns error if HTTP Request is NOT GET */
+    /* Proxy returns error if HTTP Request is not GET */
     if (strcasecmp(method, "GET")) {
         clienterror(connfd, method, "501", "Not Implemented", "Proxy does not implement this method");
         return;
@@ -81,23 +109,23 @@ void doit(int connfd) {
     parse_uri(uri, &content, &is_dynamic);
 
     /* Check if URI is in cache */
-    if (0){                                      //IMPLEMENT THIS!! :)
+    if (!is_dynamic && 0){                                      //IMPLEMENT THIS!! :)
 
 
-        char response[MAXBUF+MAX_OBJECT_SIZE];
+        ;
 
 
     }
     else{ /* If not in cache, get resource from host server */
 
         /* Parse the request headers into hdr_data and return if host header was present */
-        host_mentioned = read_requesthdrs(&rio, hdr_data);
+        host_hdr_recvd = read_requesthdrs(&rio, hdr_data);
 
         /* Begin generating new modified HTTP request to forward to the server */
         sprintf(new_request, "GET %s HTTP/1.0\r\n", content.path);
 
         /* If host was in header, use that. If not, grab host from URI */
-        if(!host_mentioned)
+        if(!host_hdr_recvd)
             sprintf(new_request, "%sHost: %s\r\n", new_request, content.host);
 
         sprintf(new_request,"%s%s%s%s%s%s", new_request, hdr_data, user_agent_hdr,
@@ -122,17 +150,15 @@ void doit(int connfd) {
 
 
         //Forward the response to the client
-        Rio_writen(connfd, response, sizeof(response));
+        Rio_writen(connfd, response, sizeof(response)); //throws warning because we don't actually write this much
         Close(clientfd);
     }
 }
 
 
 
-
-
 /*  parse_uri - read HTTP request headers */
-void parse_uri(char *uri, struct req_content *content, bool* is_dynamic)
+void parse_uri(char *uri, struct uri_content *content, bool* is_dynamic)
 {
     /* If requested resource is in the "cgi-bin" directory, then content is dynamic */
     if(strstr(uri, "cgi-bin"))
@@ -151,7 +177,7 @@ void parse_uri(char *uri, struct req_content *content, bool* is_dynamic)
         sscanf(temp,"%[^:]:%s", content->host, &content->port);
     else {
         strcpy(content->host,temp);
-        strcpy(content->port, '80');
+        strcpy(content->port, "80");
     }
 
     // in case the path to resource is empty
@@ -190,10 +216,24 @@ bool read_requesthdrs(rio_t *rp, char *header_buf)
 
 }
 
+
+/* Thread-handler that calls the required functions to serve the requests. */
+void thread_wrapper(void *vargs) {
+    int fd = *(int *) vargs;
+    Pthread_detach(pthread_self());
+    Free(vargs);
+    doit(fd);
+    Close(fd);
+    Pthread_exit(NULL);
+}
+
+void sig_handler(int sig) {
+    printf("SIGPIPE signal trapped. Exiting thread.");
+    Pthread_exit(NULL);
+}
+
 /* clienterror - returns an error message to the client */
-void clienterror(int fd, char *cause, char *errnum,
-                 char *shortmsg, char *longmsg)
-{
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg){
     char buf[MAXLINE], body[MAXBUF];
 
     /* Build the HTTP response body */
